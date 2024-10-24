018422562cbbafa423dd1e81edd1ab17e93d4ae5
Pincu Victor <andreipnk2004@gmail.com>
2024-10-24 17:28:56 +0300

    T1/C3.6

--- main.cpp
+++ main.cpp
@@ -86,7 +86,16 @@ class Level
 int main()
 {
   sf::Window window(sf::VideoMode(800, 600), "SacMan");
-
+  window.setFramerateLimit(60);
+  while (window.isOpen())
+  {
+    sf::Event event;
+    while (window.pollEvent(event))
+    {
+      if (event.type == sf::Event::Closed)
+        window.close();
+    }
+  }
 
   Helper helper;
   helper.help();
